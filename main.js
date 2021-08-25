2bb88543d32ccfe9b84d1bf93b4cea8f4d2a7679
IRWO1 <dawidekbloch@gmail.com>
2021-08-25 20:40:43 +0200

    Funkcja

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstNumber = 53 ;  
+let secondNumber = 13 ; 
+    function multiply(firstNumber, secondNumber){
+        let result  = firstNumber + secondNumber;
+        return result;
+}
+    console.log(multiply(firstNumber, secondNumber));
