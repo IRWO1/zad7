290f6d2dcba72b408c3bd6e95865d0074d8365be
IRWO1 <dawidekbloch@gmail.com>
2021-08-26 19:50:51 +0200

    ?

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let firstNumber = 53 ;  
 let secondNumber = 13 ; 
-    function multiply(firstNumber, secondNumber){
-        let result  = firstNumber + secondNumber;
-        return result;
+function multiply(firstNumber, secondNumber){
+    let result  = firstNumber + secondNumber;
+    return result;
 }
-    console.log(multiply(firstNumber, secondNumber));
+console.log(multiply(firstNumber, secondNumber));
